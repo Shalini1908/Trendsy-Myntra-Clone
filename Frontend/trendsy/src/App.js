af093ec5006de8a2e61cabc9afc94eb675d986a5
Kashif-Rezwi <kashifrezwi850@email.com>
2023-02-22 22:17:07 +0530

    Proucts page almost completed

--- Frontend/trendsy/src/App.js
+++ Frontend/trendsy/src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import {Routes , Route} from "react-router-dom";
-import Home from './Pages/Home';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import { Products } from "./Pages/Products";
 
 function App() {
   return (
     <div className="App">
-    
- <Routes>
-  <Route path='/' element={<Home/>}></Route>
- </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
     </div>
   );
 }
