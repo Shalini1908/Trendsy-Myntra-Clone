ebe9fb474785815de2c1b9ba289638f5a690e0a7
abhi5733 <abhijeetkale5733@gmail.com>
2023-02-24 23:13:40 +0530

    day-5

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-// const {authenticate}= require(".././Backend/middleware/Authentication")
+
 const { connection } = require("./config/db");
 const { dataroutes } = require("./routes/data.routes");
 const { cartroutes } = require("./routes/cart.routes");
 const { userRouter } = require("./routes/user.routes");
 const { authenticate } = require("./middleware/Authentication");
-
+const {adminRouter} = require("./routes/admin.routes")
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello trends");
 });
-app.use(authenticate);
+app.use("/admin" , adminRouter)
 app.use("/user", userRouter);
+app.use(authenticate);
 app.use("/data", dataroutes);
 app.use("/cart", cartroutes);
 
