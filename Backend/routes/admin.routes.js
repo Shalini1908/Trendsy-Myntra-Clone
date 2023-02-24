396dc9babfd87d3e6964b50cd5cbe28c7b2f3eb3
abhi5733 <abhijeetkale5733@gmail.com>
2023-02-24 17:18:28 +0530

    day-4

---
+++ Backend/routes/admin.routes.js
@@ -0,0 +1,72 @@
+const { adminModel } = require("../model/admin.model")
+
+const express = require("express")
+
+const adminRouter = express.Router()
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+
+
+
+adminRouter.post("/register", async (req, res) => {
+
+    const { pass, email, icon } = req.body
+
+
+    try {
+
+        bcrypt.hash(pass, 10, async (err, hash) => {
+
+            if (err) {
+                console.log(err)
+            }
+            const user = new adminModel({ pass: hash, email, icon })
+            await user.save()
+            res.send("admin registered successfully")
+
+        })
+
+    } catch (err) {
+
+        res.send("admin registeration failed")
+    }
+
+
+
+})
+
+
+adminRouter.post("/login", async (req, res) => {
+
+    const { email, pass } = req.body
+
+    try {
+        let user = await adminModel.find({ email: email})
+
+        if (user.length === 0) {
+            return res.send({ "msg": "admin not found" })
+        }
+
+        bcrypt.compare(pass, user[0].pass, (err, result)=> {
+         
+            if(result){
+                const token = jwt.sign({ userID: user[0]._id  }, 'masai');
+                
+         res.send({"msg":"admin login successfull","token":token})
+            }else{
+                res.send({"err":"login failed"})
+            }
+    
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.send({ "msg": "login failed" })
+    }
+
+
+
+})
+
+module.exports = { adminRouter }
